5c49be945cc50cba08b72d5761793eaed53cdd43
svineet <svineet@cmc.edu>
2020-02-12 20:53:23 -0800

    Modified the HTML_Validator python file.

--- HTML_Validator.py
+++ HTML_Validator.py
@@ -10,6 +10,20 @@ def validate_html(html):
     >>> validate_html('<strong>example')
     False
     '''
+    s = _extract_tags(html)
+    if len(s) < 2:
+        return False
+    #print(s)
+    left = 0
+    right = len(s)-1
+    while (left < right):
+        open = s[left]
+        close = s[right]
+        if open[1:-1] != close[2:-1]:
+            return False
+        right -= 1
+        left += 1
+    return True
 
     # HINT:
     # use the _extract_tags function below to generate a list of html tags without any extra text;
@@ -29,3 +43,16 @@ def _extract_tags(html):
     >>> _extract_tags('Python <strong>rocks</strong>!')
     ['<strong>', '</strong>']
     '''
+
+    s=[]
+    for i in range(len(html)):
+        temp = ''
+        symbol = html[i]
+        if symbol == '<' :
+            while symbol != '>':
+                temp += symbol
+                i = i+1
+                symbol = html[i]
+            temp += '>'
+            s.append(temp)
+    return s
